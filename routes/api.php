5808dac8060f08add4d268609c756157a0b97b9c
kablin <rezcopy@rambler.ru>
2025-02-17 13:23:13 +0300

    auth

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use App\Services\JobsService;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,13 +21,49 @@ Route::get('/lock_create', function (Request $request) {
 
 
 
+
+Route::post('/v1/set_callback', function (Request $request) {
+    try {
+        $callback = json_decode($request->getContent())->callback;
+        if ($callback) {
+            $request->user()->callback = $callback;
+            $request->user()->save();
+            return json_encode(['status' => true]);
+        } else   return json_encode(['status' => false]);
+    } catch (Exception $e) {
+        return json_encode(['status' => false]);
+    }
+})->middleware('auth:sanctum');
+
+
+
+
+Route::post('/v1/get_token', function (Request $request) {
+    try {
+        $credentials['email'] = json_decode($request->getContent())->email;
+        $credentials['password'] = json_decode($request->getContent())->password;
+
+        if (Auth::attempt($credentials)) {
+            $token = auth()->user()->createToken('ttlock');
+            return json_encode(['token'=>$token->plainTextToken,'status' => true, 'user_id'=> auth()->user()->id]);
+        }
+        else   return json_encode(['status' => false]);
+
+    } catch (Exception $e) {
+        return json_encode(['status' => false]);
+    }
+});
+
+
+
 Route::post('/v1/get_lock_list', function (Request $request) {
     (new JobsService(1))->getLockList();
 });
 
 
+
 Route::post('/v1/add_code_to_lock', function (Request $request) {
-    (new JobsService(1))->addKeyToLock($request->lock_id,$request->code, $request->begin,$request->end);
+    (new JobsService(1))->addKeyToLock($request->lock_id, $request->code, $request->begin, $request->end);
 });
 
 
@@ -38,10 +76,10 @@ Route::post('/v1/set_lock_passage_mode_off', function (Request $request) {
 });
 
 Route::post('/v1/delete_code_from_lock', function (Request $request) {
-    (new JobsService(1))->deleteKey($request->lock_id,$request->code_id);
+    (new JobsService(1))->deleteKey($request->lock_id, $request->code_id);
 });
 
 
 Route::post('/callback', function (Request $request) {
-    info('callback',$request->all());
- });
+    info('callback', $request->all());
+});
