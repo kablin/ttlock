385b0132d4157e3f4696c89a33a11c7d8faa667d
kablin <rezcopy@rambler.ru>
2026-02-19 19:05:13 +0300

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -81,7 +81,7 @@ class AuthController extends Controller
                 if ($user->wasRecentlyCreated) {
                     $code_packet = CodePacket::firstOrCreate(['user_id' => $user->id]);
                     $code_packet->refresh();
-                    $code_packet->count = 10;
+                    $code_packet->count = 30;
 
                     $code_packet->end = $code_packet->created_at->addYear();
                     $code_packet->save();
