6ba495332f314fb23291d26d80716df51a795cf6
kablin <rezcopy@rambler.ru>
2026-05-17 12:55:50 +0300

    fix

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,13 +5,56 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { initializeTheme } from './composables/useAppearance';
-
+import axios from 'axios';
 import '@fontsource/inter/400.css'    // normal
 import '@fontsource/inter/500.css'    // medium
 import '@fontsource/inter/600.css'    // semi-bold
 import '@fontsource/inter/700.css'    // bold
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+
+
+axios.defaults.withCredentials = true;
+
+
+axios.interceptors.response.use(
+    (res) => res,
+    async (err) => {
+        const status = err.response?.status;
+        const originalRequest = err.config;
+
+        // 1. Флаг защиты от бесконечного цикла
+        // Если запрос уже помечен как повторный, не обрабатываем его снова
+        if (originalRequest._retry) {
+            return Promise.reject(err);
+        }
+
+        // 2. Обрабатываем только нужные ошибки
+        if (status === 401 || status === 419) {
+            console.log('reload '+status)
+            originalRequest._retry = true; // Помечаем, что этот запрос сейчас будем повторять
+
+            try {
+                // 3. Тихо обновляем сессию/токен
+                await axios.get('/session/refresh');
+                
+                // 4. ПОВТОРНО ОТПРАВЛЯЕМ исходный запрос и ВОЗВРАЩАЕМ его результат
+                // axios(originalRequest) автоматически подхватит новый CSRF-токен из куки
+                return axios(originalRequest);
+                
+            } catch (refreshError) {
+                // Если обновление сессии не удалось 
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(err);
+    }
+);
+
+
+
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Middleware\CodesCounter;
+use Illuminate\Http\Request;
 Route::get('/', function () {
   return Inertia::render('Welcome');
 })->name('home');
@@ -161,14 +162,18 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 
-
+/*
 Route::get('/test', [\App\Http\Controllers\LockController::class, 'test'])->name('test2');
 Route::get('/testdel', [\App\Http\Controllers\LockController::class, 'testdel'])->name('testdel');
 Route::get('/testlist', [\App\Http\Controllers\LockController::class, 'testlist'])->name('testlist');
+*/
 
 
+Route::get('/session/refresh', function (Request $request) {
+     $request->session()->regenerateToken();
 
-
+    return response()->json();
+})->middleware('web'); 
 
 
 
